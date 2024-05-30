fd1e33a673586ec86c914e3ec8691da3323626a5
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2024-05-30 15:52:20 -0300

    Initial commit

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetearVehiculos();
+        }
+    }
+
+    void ResetearVehiculos()
+    {
+        DeactivateVehiculos();
+        vehiculos[Random.Range(0,6)].SetActive(true);
+    }
+    void DeactivateVehiculos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+}
